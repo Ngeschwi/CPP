826346ba61c83dd7b9d4ec22eba1c122b946c893
Nathan Geschwind <ngeschwi@z4r7p7.42lyon.fr>
2022-06-02 17:19:47 +0200

    increase

--- Module 04/ex00/main.cpp
+++ Module 04/ex00/main.cpp
@@ -6,22 +6,34 @@
 
 int	 main()
 {
+	std::cout << "---Animal : ---" << std::endl;
 	const Animal* meta =  new Animal();
 	const Animal* cat = new Cat();
 	const Animal* dog = new Dog();
 
+	std::cout << "-type:" << std::endl;
 	std::cout << cat->getType() << std::endl;
 	std::cout << dog->getType() << std::endl;
+	std::cout << std::endl;
 
+	std::cout << "-makesound:" << std::endl;
 	cat->makeSound();
 	dog->makeSound();
 	meta->makeSound();
+	std::cout << std::endl;
 
+	std::cout << "---Wrong Animal : ---" << std::endl;
 	const WrongAnimal* wrong = new WrongAnimal();
 	const WrongAnimal* wrongcat = new WrongCat();
 
+	std::cout << "-type:" << std::endl;
+	std::cout << wrongcat->getType() << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "-makesound:" << std::endl;
 	wrong->makeSound();
 	wrongcat->makeSound();
+	std::cout << std::endl;
 
 	delete meta;
 	delete cat;
