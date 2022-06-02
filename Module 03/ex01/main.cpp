96c3458a995d95d878f560f9d4eed41b942f3d11
Nathan Geschwind <ngeschwi@z4r7p7.42lyon.fr>
2022-06-02 17:00:07 +0200

    increase

--- Module 03/ex01/main.cpp
+++ Module 03/ex01/main.cpp
@@ -4,21 +4,25 @@
 int	main()
 {
 	ClapTrap	clap_A("Bob");
-	ScavTrap	scav_A("Bryan");
+	ScavTrap	scav_A("Bob");
 
+	std::cout << "--- Claptrap : ---" << std::endl;
 	clap_A.attack("Jean");
-	std::cout << std::endl;
-	scav_A.attack("George");
 	std::cout << std::endl;
 	clap_A.takeDamage(6);
 	std::cout << std::endl;
-	scav_A.takeDamage(6);
-	std::cout << std::endl;
 	clap_A.beRepaired(10);
+	std::cout << std::endl << std::endl;
+
+	std::cout << "--- Scavtrap : ---" << std::endl;
+	scav_A.attack("Jean");
+	std::cout << std::endl;
+	scav_A.takeDamage(6);
 	std::cout << std::endl;
 	scav_A.beRepaired(10);
 	std::cout << std::endl;
 	scav_A.guardGate();
+	std::cout << std::endl << std::endl;;
 
 	return 0;
 }
